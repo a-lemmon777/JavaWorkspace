0ad66fe7d19b6df76edb6869ae74cbbe3c99c56c
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-09-20 22:48:48 -0500

    Tried a lot of things in JavaQuirks

--- JavaQuirks/SomeJavaTesting.java
+++ JavaQuirks/SomeJavaTesting.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 
 public class SomeJavaTesting {
 
+	private int coolInt;
+	private char defaultChar;
+//	final int neverChanged2; // The declaration throws a compilation error early
+
 	@Test
 	public void test() {
 		assertTrue(true);
@@ -45,6 +49,10 @@ public class SomeJavaTesting {
 		assertEquals(-9223372036854775808L, myLong);
 	}
 	
+	@Test (expected = Error.class)
+	public void badLongLiteral() {
+		long myLong = 9223372036854775807;
+	}
 	@Test
 	public void upConvert() {
 		byte myByte = 5;
@@ -54,21 +62,72 @@ public class SomeJavaTesting {
 		assertEquals(5, myLong);
 	}
 	
-//	@Test
-//	public void cantDownConvert() {
-//		int myInt = 5;
-//		short myShort = myInt;
-//	}
+	@Test (expected = Error.class)
+	public void cantDownConvert() {
+		int myInt = 5;
+		short myShort = myInt;
+	}
 	
-//	@Test
-//	public void cantDownCovertToByte() {
-//		byte myByte = 2349087;
-//	}
+	@Test
+	public void downCast() {
+		int myInt = 5;
+		short myShort = (short) myInt;
+	}
 	
-//	@Test
-//	public void stillCantDownConvertToByte() {
-//		//byte myByte = 128;
-//	}
+	@Test
+	public void downCastAndRollsOver() {
+		int myInt = 128;
+		byte myByte = (byte) myInt;
+		assertEquals(-128, myByte);
+	}
 	
+	@Test (expected = Error.class)
+	public void cantDownCovertToByte() {
+		byte myByte = 2349087;
+	}
+	
+	@Test (expected = Error.class)
+	public void stillCantDownConvertToByte() {
+		byte myByte = 128;
+	}
+	
+	@Test (expected = Error.class)
+	public void doubleToInt() {
+		double thing = -5.5;
+		int intThing = thing; // Type mismatch compilation error
+	}
+	
+	@Test
+	public void absOfMostNegativeNumber() {
+		assertEquals(Integer.MIN_VALUE, Math.abs(Integer.MIN_VALUE));
+	}
+	
+	@Test
+	public void defaultIntFieldValue() {
+		assertEquals(0, coolInt);
+	}
+	
+	@Test (expected = Error.class)
+	public void noDefaultVariableValue() {
+		int unCoolInt;
+		assertEquals(0, unCoolInt); // Throws Unresolved compilation error
+	}
+	
+	@Test
+	public void defaultCharFieldValue() {
+		assertEquals('\0', defaultChar);
+		assertEquals('\u0000', defaultChar);
+	}
+	
+	@Test
+	public void uniCodeLiterals() {
+		int \u00ED = 5;
+		assertEquals(\u00ed, \u00ed);
+	}
+	
+	@Test
+	public void notAssigningAValueToFinal() {
+		final int neverChanged; // no compilation error
+	}
 	
 }
--- JavaQuirks/oddsNEnds.java
+++ JavaQuirks/oddsNEnds.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+
 public class oddsNEnds {
+	
+	private static int[] temp;
+	
 	public static void main(String[] args) {
 		System.out.println("a\nb\nc\n");
 		System.out.println("There should be an empty line here ^^");
@@ -7,26 +12,30 @@ public class oddsNEnds {
 		char myQuote = '\'';
 		System.out.println(0x00);
 		System.out.println(0xFF);
+		// Octal literals
 		System.out.println(000);
 		System.out.println(077);
+		System.out.println(00.77); // Octal literals dont work for floating point numbers
 		System.out.println(0b1000);
-		//System.out.println(0b1002); // Unuseful error message.
+//		System.out.println(0b1002); // Unuseful error message.
 		//System.out.println(088); // Doesn't work, expecting digits to be 0 to 7
-		//final int MY_AGE = 23;
-		//MY_AGE++; // Can't change a final variable.
-		//System.out.println(MY_AGE);
+		final int MY_AGE = 23;
+//		MY_AGE++; // Can't change a final variable.
+//		System.out.println(MY_AGE);
 		double thing1 = -5.5;
+		// cast truncates toward 0
 		int thing2 = (int) thing1;
 		System.out.println(thing2);
 		final int DAYS_PER_WEEK = 7;
 		double[] maxTemps = new double[DAYS_PER_WEEK];
 		int thing3 = 2;
-		//int thing4 = ++thing3++; // The postfix happens first, which returns a value. The prefix ++ is expecting a variable not a value, so it gets messed up.
-		//System.out.println(thing4);
+//		int thing4 = ++thing3++; // The postfix happens first, which returns a value. The prefix ++ is expecting a variable not a value, so it gets messed up.
+//		System.out.println(thing4);
 		String[] stuNames;
 		stuNames = new String[10];
-		stuNames[0] = new String("Andrew");
+		stuNames[0] = "Andrew";
 		System.out.println(stuNames);
+		System.out.println(stuNames.toString());
 		System.out.println(stuNames[0]);
 		final int WEEKS_PER_YEAR = 52;
 		double[][] minTemps = new double[DAYS_PER_WEEK][WEEKS_PER_YEAR];
@@ -47,6 +56,30 @@ public class oddsNEnds {
 		System.out.println('\141');
 		System.out.println('\377');
 		System.out.println('\377');
-		
+		temp = new int[8];
+		doStuff(); // returns a value and does nothing
+		doArrayStuff(); // assigns to temp[0], it knows temp was initialized earlier.
+		int length2 = new double[4].length;
+//		int number = doMoreArrayStuff({3, 4, 5}); // bracket notation is only for initialization
+		char[] someChars;
+//		someChars = {'a', 'b','c'}; // can only be used in initializers
+		int[] coolInts = {freq[2], 5};
+		int[] unCoolInts = {5, freq[2]};
+		int[] someInts = {5, freq[2]}; // values can be provided at runtime, it appears
+		System.out.println(coolInts.equals(unCoolInts));
+		System.out.println(someInts.equals(unCoolInts)); // Different objects, same values, returns false
+		System.out.println(Arrays.equals(someInts, unCoolInts)); // Different objects, same values, returns true
+	}
+
+	private static int doMoreArrayStuff(int[] is) {
+		return 0;
+	}
+
+	private static void doArrayStuff() {
+			temp[0] = 8;
+	}
+
+	private static int doStuff() {
+		return 5;
 	}
 }
