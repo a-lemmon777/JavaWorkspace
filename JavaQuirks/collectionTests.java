190a6c12b9003943a86c6686df0365ec6822c49e
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-09-22 00:22:37 -0500

    Worked on some JavaQuirks

--- JavaQuirks/collectionTests.java
+++ JavaQuirks/collectionTests.java
@@ -6,6 +6,32 @@ import org.junit.Test;
 public class collectionTests {
 
 	@Test
+	public void arrayDeclaration() {
+		int[] niceInts = new int[3];
+		niceInts[0] = 5;
+		assertEquals(5, niceInts[0]);
+	}
+	
+	@Test
+	public void otherArrayDeclaration() {
+		int[] niceInts = {1, 2, 3};
+		assertEquals(1, niceInts[0]);
+	}
+	
+	@Test (expected = Error.class)
+	public void reallyQuickInitializationWontWork() {
+		int[] niceInts;
+		niceInts = {1, 2, 3};
+	}
+	
+	@Test
+	public void kindaQuickInitializationWorks() {
+		int[] niceInts;
+		niceInts = new int[] {1, 2, 3};
+		assertEquals(1, niceInts[0]);
+	}
+	
+	@Test
 	public void testOrdering() {
 		String[][] coolArray;
 		coolArray = new String[5][3];
@@ -21,9 +47,38 @@ public class collectionTests {
 	}
 	
 	@Test
-	public void test() {
+	public void testMultiDimArrays() {
 		String[][] coolArray;
 		coolArray = new String[5][]; // Yes!!!! I dont have to initialize subarrays!!!!
 		assertArrayEquals(null, coolArray[0]);
+	}
+	
+	@Test
+	public void arraysCanBeStoredAsObjects() {
+		Object thing = new int[5];
+		System.out.println(((int[]) thing)[0]);
+	}
+	
+	@Test
+	public void anArrayOfObjectsCanHoldOtherArrays() {
+		Object[] thing = new Object[5];
+		thing[0] = new int[3];
+		thing[1] = new double[3];
+	}
+	
+	@Test (expected = Error.class)
+	public void anArrayOfObjectsNeedCastingToWorkWith() {
+		Object[] thing = new Object[5];
+		thing[0] = new int[3];
+		thing[1] = new double[3];
+		thing[0][0] = 5;
+	}
+	
+	@Test
+	public void anArrayOfObjectsNeedCastingToWorkWithHeresACast() {
+		Object[] thing = new Object[5];
+		thing[0] = new int[3];
+		thing[1] = new double[3];
+		((int[])thing[0])[0] = 5;
 	}
 }
