e7e70b1eb68ae0fd659cffd4c54a19ba78ff47cd
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-09-22 19:29:20 -0500

    Did stuff and added workspace metadata

--- JavaQuirks/WrapperTests.java
+++ JavaQuirks/WrapperTests.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
@@ -40,16 +43,35 @@ public class WrapperTests {
 		String first = "hi";
 		String second = "hi";
 		assertTrue(first == second);
-		String third = String.valueOf('h') + String.valueOf('i');
+		String third = String.valueOf('h') + String.valueOf('i'); // doesn't get pooled
+		String fourth = new String("hi"); // doesn't get pooled
 		assertFalse(first == third);
+		assertFalse(first == fourth);
 	}
 	
-//	@Test
-//	public void testIntegerPooling() {
-//		Integer first = 1;
-//		Integer second = 1;
-//		assertTrue(first == second);
-//		Integer third = Integer.valueOf(1);
-//		assertFalse(first == third);
-//	}
+	@Test
+	public void testIntegerPooling() {
+		Integer first = 1;
+		Integer second = 1;
+		assertTrue(first == second);
+		Integer third = new Integer(1); // doesn't get pooled
+		int i = 1;
+		Integer fourth = 0 + 1; // gets pooled, since autoboxing occurs and fits in a byte
+		assertFalse(first == third);
+		assertTrue(first == fourth);
+		Integer fifth = 128; // not pooled, over 127
+		Integer sixth = 128;
+		assertFalse(fifth == sixth);
+	}
+	
+	@Test
+	public void testBoxing() {
+		Integer first = 5;
+		int i = 1;
+		Integer second = i;
+		List<Integer> myList = new ArrayList<Integer>();
+		
+		
+		
+	}
 }
