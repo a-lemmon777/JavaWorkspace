fa7cd12e37c33014fdc66811e0c085850eae4747
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-10-01 17:00:11 -0500

    Finished Data Structures Take Home Exam 1

--- TakeHomeOne.java
+++ TakeHomeOne.java
@@ -13,37 +13,61 @@ public class TakeHomeOne {
 		ArrayList<Integer> input1 = new ArrayList<Integer>();
 		ArrayList<Integer> input2 = new ArrayList<Integer>();
 		ArrayList<Integer> expectedResult = new ArrayList<Integer>();
+		ArrayList<Integer> input1AfterSideEffects = new ArrayList<Integer>(input1);
+		ArrayList<Integer> input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		// Edge case where the first input is empty
 		input1 = new ArrayList<Integer>(Arrays.asList(5));
 		input2 = new ArrayList<Integer>();
 		expectedResult = new ArrayList<Integer>();
+		input1AfterSideEffects = new ArrayList<Integer>(input1);
+		input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		// Edge case where the second input is empty
 		input1 = new ArrayList<Integer>();
 		input2 = new ArrayList<Integer>(Arrays.asList(5));
 		expectedResult = new ArrayList<Integer>();
+		input1AfterSideEffects = new ArrayList<Integer>(input1);
+		input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		// Case where inputs have some common values
 		input1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
 		input2 = new ArrayList<Integer>(Arrays.asList(4, 5, 6));
 		expectedResult = new ArrayList<Integer>(Arrays.asList(4, 5));
+		input1AfterSideEffects = new ArrayList<Integer>(input1);
+		input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		// Case where inputs have some common values (with multiple instances)
 		input1 = new ArrayList<Integer>(Arrays.asList(7, 5, 2, 7, 6));
 		input2 = new ArrayList<Integer>(Arrays.asList(5, 7, 7, 1, 0, 2, 7));
 		expectedResult = new ArrayList<Integer>(Arrays.asList(7, 5, 2, 7));
+		input1AfterSideEffects = new ArrayList<Integer>(input1);
+		input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		// Case where inputs have values, but no common values
 		input1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
 		input2 = new ArrayList<Integer>(Arrays.asList(4, 5, 6));
 		expectedResult = new ArrayList<Integer>();
+		input1AfterSideEffects = new ArrayList<Integer>(input1);
+		input2AfterSideEffects = new ArrayList<Integer>(input2);
 		System.out.println(expectedResult.equals(listIntersection(input1, input2)));
+		System.out.println(input1AfterSideEffects.equals(input1));
+		System.out.println(input2AfterSideEffects.equals(input2));
 		
 		/* Testing for stackToArray() */
 		System.out.println("Testing stackToArray(), all lines should print true:");
@@ -51,7 +75,10 @@ public class TakeHomeOne {
 		// Edge case where the input is empty
 		Stack<Integer> input = new Stack<Integer>();
 		int[] expectedResultArray = new int[0];
+		Stack<Integer> inputAfterSideEffects = new Stack<Integer>();
+		inputAfterSideEffects.addAll(input);
 		System.out.println(Arrays.equals(expectedResultArray, stackToArray(input)));
+		System.out.println(inputAfterSideEffects.equals(input));
 		
 		// Case where the stack has some values
 		input = new Stack<Integer>();
@@ -61,15 +88,18 @@ public class TakeHomeOne {
 		input.push(8);
 		input.push(12);
 		expectedResultArray = new int[] {9, 3, 4, 8, 12};
+		inputAfterSideEffects = new Stack<Integer>();
+		inputAfterSideEffects.addAll(input);
 		System.out.println(Arrays.equals(expectedResultArray, stackToArray(input)));
+		System.out.println(inputAfterSideEffects.equals(input));
 		
-		/* Testing for removeAlphabetically */
-		System.out.println("Testing removeAlphabetically(), all lines should print true:");
+		/* Testing for removeAlphabeticallyLast */
+		System.out.println("Testing removeAlphabeticallyLast(), all lines should print true:");
 		
 		// Edge case where the input is empty
 		Stack<String> inputStack = new Stack<String>();
 		Stack<String> expectedResultStack = new Stack<String>();
-		removeAlphabetically(inputStack);
+		removeAlphabeticallyLast(inputStack);
 		System.out.println(expectedResultStack.equals(inputStack));
 		// Case where the input has only one instance of the last-alphabetical-string
 		inputStack = new Stack<String>();
@@ -79,7 +109,7 @@ public class TakeHomeOne {
 		expectedResultStack = new Stack<String>();
 		expectedResultStack.push("aaa");
 		expectedResultStack.push("bbb");
-		removeAlphabetically(inputStack);
+		removeAlphabeticallyLast(inputStack);
 		System.out.println(expectedResultStack.equals(inputStack));
 		// Case where the input has multiple instances of the last-alphabetical-string
 		inputStack = new Stack<String>();
@@ -91,13 +121,15 @@ public class TakeHomeOne {
 		expectedResultStack = new Stack<String>();
 		expectedResultStack.push("mmm");
 		expectedResultStack.push("www");
-		removeAlphabetically(inputStack);
+		removeAlphabeticallyLast(inputStack);
 		System.out.println(expectedResultStack.equals(inputStack));
+		
+		// In all of these cases the program ran as expected.
 	}
 	
 	public static ArrayList<Integer> listIntersection(ArrayList<Integer> arrayList1, ArrayList<Integer> arrayList2) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
-		ArrayList<Integer> list2 = new ArrayList<Integer>(arrayList2); // makes a copy of arrayList2 so the inputs don't get modified
+		ArrayList<Integer> list2 = new ArrayList<Integer>(arrayList2); // makes a copy of arrayList2 so no side-effecting occurs
 		// add "matches" to the result and remove from list2 to avoid double-counting
 		for (int number : arrayList1) {
 			int foundIndex = list2.indexOf(number);
@@ -116,20 +148,18 @@ public class TakeHomeOne {
 			temp.push(stack.pop());
 			count++;
 		}
-		// declare an int array big enough to hold them all
+		// declare an int array big enough to hold all elements
 		int[] result = new int[count];
-		// do a for loop that moves each thing back to the original stack and also puts it in the array
+		// move each element back to the original stack and copy it to the array
 		for (int i = 0; i < count; i++) {
 			int element = temp.pop();
 			result[i] = element;
 			stack.push(element);
 		}
-		// return the resulting array
-		return result;	
+		return result;
 	}
 	
-	public static void removeAlphabetically(Stack<String> stack) {
-		// declare a temporary stack
+	public static void removeAlphabeticallyLast(Stack<String> stack) {
 		Stack<String> temp = new Stack<String>();
 		// empty the stack into the temp stack, keeping track of the "greatest" string
 		String lastAlphabetically = "";
@@ -140,8 +170,7 @@ public class TakeHomeOne {
 			}
 			temp.push(current);
 		}
-		// put the temporary stack back into the original, each time checking if it's the "greatest" string
-		// if it's not the "greatest" string, push it back to the original
+		// push each element in temp back into the original stack, but only if the element isn't the "greatest" string
 		while (!temp.empty()) {
 			String current = temp.pop();
 			if (!current.equals(lastAlphabetically)) {
@@ -149,5 +178,4 @@ public class TakeHomeOne {
 			}
 		}
 	}
-	
 }
