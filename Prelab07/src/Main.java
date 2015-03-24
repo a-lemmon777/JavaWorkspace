97531a47eb7579e008dd275e905b88ac699d6297
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-03-23 19:33:15 -0500

    Finished Prelab07

--- Prelab07/src/Main.java
+++ Prelab07/src/Main.java
@@ -3,29 +3,31 @@ import java.util.Random;
 
 public class Main {
     
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException {
        Random random = new Random();
        ArrayList<Integer> list1 = new ArrayList<Integer>();
        for (int i = 0; i < 10000; i++) {
            list1.add(random.nextInt(10000));
        }
-       System.out.println(searchArray(list1.get(1), list1));
-       System.out.println(searchArray(list1.get(5), list1));
-       System.out.println(searchArray(list1.get(900), list1));       
-       System.out.println(searchArray(list1.get(3200), list1));
-       System.out.println(searchArray(list1.get(7400), list1));
-       System.out.println(searchArray(list1.get(9876), list1));
-       System.out.println(searchArray(2000000, list1));
-       System.out.println(searchArray(-45, list1));
+       System.out.println(searchArray(list1.get(0), list1)); // Should print true
+       System.out.println(searchArray(list1.get(1), list1)); // Should print true
+       System.out.println(searchArray(list1.get(5), list1)); // Should print true
+       System.out.println(searchArray(list1.get(900), list1)); // Should print true 
+       System.out.println(searchArray(list1.get(3200), list1)); // Should print true
+       System.out.println(searchArray(list1.get(7400), list1)); // Should print true
+       System.out.println(searchArray(list1.get(9876), list1)); // Should print true
+       System.out.println(searchArray(list1.get(9999), list1)); // Should print true
+       System.out.println(searchArray(2000000, list1)); // Should print false
+       System.out.println(searchArray(-45, list1)); // Should print false
    }
    
-   private static boolean searchArray(int target, ArrayList<Integer> list) {
+   private static boolean searchArray(int target, ArrayList<Integer> list) throws InterruptedException {
        ThreadedSearch<Integer> searcher = new ThreadedSearch<Integer>();
        // This specifies 4 threads for the tests. It would be a good idea to play
        // with this and see how that changes things. Keep in mind that your number
        // of threads *may* need to evenly divide the length of the list being
        // searched (10,000 in this case).
-       return searcher.parallelSearch(4, target, list);
+       return searcher.parallelSearch(7, target, list);
    }
 
 }
--- Prelab07/src/ThreadedSearch.java
+++ Prelab07/src/ThreadedSearch.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class ThreadedSearch<T> {
+public class ThreadedSearch<T> implements Runnable {
     
     private T storedTarget;
     private ArrayList<T> storedList;
@@ -9,28 +9,65 @@ public class ThreadedSearch<T> {
     private Answer storedAnswer;
     
     public ThreadedSearch() {
-        //this is the base class
+    	//this is the base constructor
+    	storedAnswer = new Answer();
     }
     
     private ThreadedSearch(T target, ArrayList<T> list, int start, int end, Answer answer) {
         storedTarget = target;
         storedList = list; 
         storedStart = start;
-        storedEnd = end;
+        storedEnd = end; // End is exclusive
         storedAnswer = answer;
     }
     
     //assume list size is divisible by numThreads to start
-    public boolean parallelSearch(int numThreads, T target, ArrayList<T> list) {
-        return true;
+    public boolean parallelSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
+    	numThreads = Math.min(numThreads, list.size()); // Doesn't make sense to have more threads than items in list.
+    	Thread[] threads = new Thread[numThreads];
+    	for (int i = 0; i < numThreads; i++) {
+    		int startIndex = getStartIndex(i, numThreads, list.size());
+    		int endIndex = getEndIndex(i, numThreads, list.size());
+    		threads[i] = new Thread(new ThreadedSearch<T>(target, list, startIndex, endIndex, storedAnswer));
+    		threads[i].start();
+    	}
+    	
+    	for (int i = 0; i < numThreads; i++) {
+    		threads[i].join();
+    	}
+    	
+        return storedAnswer.answer;
     }
     
-    private void search(T target, ArrayList<T> list, int start, int end) {
-        
-    }
-    
-    private class Answer {
-        public boolean answer = false;
-    }
+    private int getStartIndex(int currentThread, int numThreads, int arraySize) {
+    	int chunkSize = divideAndRoundUp(arraySize, numThreads);
+		return currentThread * chunkSize;
+	}
+
+    private int getEndIndex(int currentThread, int numThreads, int arraySize) {
+    	int chunkSize = divideAndRoundUp(arraySize, numThreads);
+    	int endIndex = (currentThread + 1) * chunkSize; // endIndex is exclusive
+    	return Math.min(endIndex, arraySize); // Prevents out of bounds exceptions
+	}
+
+	// Just trying to divide and round up here... couldn't find a better way
+	private int divideAndRoundUp(int x, int y) {
+		return x % y == 0 ? x / y : (x / y) + 1;
+	}
+
+	@Override
+	public void run() {
+		for(int i = storedStart; i < storedEnd; i++) {
+            if(storedList.get(i).equals(storedTarget)) {
+                storedAnswer.answer = true;
+                return; // Optimizes speed if the item is found early.
+            }
+        }
+    	// If not found, do nothing, and the value in storedAnswer stays false
+	}
+
+	private class Answer {
+	    public boolean answer = false;
+	}
 
 }
