2bf42086cc27298c09b09c2bfa71abdc85343e9a
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-10-21 21:20:02 -0500

    Finished sorting competition, not very fast though...

--- LemmonSort2015/src/Group16.java
+++ LemmonSort2015/src/Group16.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class TimSortInt {
+public class Group16 {
 
 	public static void main(String[] args) throws InterruptedException {
 		if (args.length < 2) {
@@ -82,7 +82,7 @@ public class TimSortInt {
 		}
 	}
 	
-	public static class ModData implements Comparable<ModData> {
+	public static class ModData {
 		public String fullString;
 		public int modValue;
 		public int integerValue;
@@ -93,17 +93,6 @@ public class TimSortInt {
 			integerValue = (input.charAt(2) - 48) * 100000000 + (input.charAt(3) - 48) * 10000000 + (input.charAt(4) - 48) * 1000000 +
 					(input.charAt(5) - 48) * 100000 + (input.charAt(6) - 48) * 10000 + (input.charAt(7) - 48) * 1000 + (input.charAt(8) - 48) * 100 +
 					(input.charAt(9) - 48) * 10 + (input.charAt(10) - 48);
-		}
-
-		@Override
-		public int compareTo(ModData value2) {
-			int prefixDifference = value2.modValue - this.modValue;
-			if (prefixDifference != 0) {
-				// negative iff this should precede value2
-				return prefixDifference;
-			}
-			// negative iff this should precede value2
-			return this.integerValue - value2.integerValue;
 		}
 	}
 	
